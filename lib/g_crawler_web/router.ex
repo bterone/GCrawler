8781954bc61f9b18a984b185b3b85824666addbe
Terone <bterone@gmail.com>
2020-04-15 10:40:04 +0700

    Add path to create user and add to session current user

--- lib/g_crawler_web/router.ex
+++ lib/g_crawler_web/router.ex
@@ -17,7 +17,7 @@ defmodule GCrawlerWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
-    resources "/users", UserController, only: [:create, :new]
+    resources "/registrations", UserController, only: [:create, :new]
   end
 
   # Other scopes may use custom stacks.
